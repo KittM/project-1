64a10d27b79346ee13076c0679abb1dd43a6cf13
Tamar Cabayan <tcabayan@gmail.com>
2020-04-02 23:07:42 -0700

    deleted .gitignore

---
+++ api-keys.js
@@ -0,0 +1,5 @@
+var unsplashkey = "y38Jk_LOKuPLJm-3Z_M-5VDk9rDRIpwySCrdSIcevJE"
+
+var firekey = "AIzaSyB-nMolmk62DCS_T9_DbsW5GXNOQh8OIK8"
+
+var quotekey = "132b64a313a114bf89a35ae9cbf40d587aa90a41"
